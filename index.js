d1592017dede78dee4b9c4f8139ad9c37e926db1
Edward Jin <57547638+EddieJ03@users.noreply.github.com>
2022-12-14 16:52:55 -0800

    added default route to check deployment

--- index.js
+++ index.js
@@ -479,17 +479,9 @@ app.delete("/delete", AuthMiddleware, async (req, res) => {
 
 //-------------------DEPLOYMENT----------------//
 // ORDER MATTERS! THIS MUST BE BELOW ALL ROUTES ABOVE
-// if (process.env.NODE_ENV === "production") {
-//     app.use(express.static(path.join(__dirname, "/client/build")));
-  
-//     app.get("*", (req, res) =>
-//       res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-//     );
-// } else {
-//     app.get("/", (req, res) => {
-//       res.send("API is running...");
-//     });
-// }
+app.get("/", (req, res) => {
+    res.send("API is running...");
+});
 
 server.listen(PORT, () => {
     console.log('server running on PORT ' + PORT)
